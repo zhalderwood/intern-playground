69f75abc294d56e8a695a7762630ff09fa7bdc1b
zhalderwood <zju.0228@gmail.com>
2021-06-08 17:58:08 -0500

    add API fns for get, post, patch

---
+++ CityInfo/CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
+    }
+}
